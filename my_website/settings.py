7bbb9306e3d289840546dcbbba5bb67731673d22
ismail-nijazi <ismail.nijazi123@gmail.com>
2021-11-14 11:04:12 +0100

    11:03 14 Nov

--- my_website/settings.py
+++ my_website/settings.py
@@ -166,3 +166,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILES_OVERWRITE = False
 AWS_DEFUAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_S3_REGION_NAME = 'eu-north-1' #change to your region
+AWS_S3_SIGNATURE_VERSION = 's3v4'
